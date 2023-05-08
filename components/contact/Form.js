62503047bdf795f9815294f09bf86b16c094a76c
madmonkey <104393122+phillipc421@users.noreply.github.com>
2023-05-07 19:45:59 -0500

    form validation

--- components/contact/Form.js
+++ components/contact/Form.js
@@ -1,17 +1,27 @@
 import { useState } from "react";
-import { Box, TextField, Button } from "@mui/material";
+import { Box, TextField, Button, Snackbar } from "@mui/material";
+const defaults = { name: "", email: "", orderNumber: "", message: "" };
 export default function Form() {
-  const [formFields, setFormFields] = useState({
-    name: "",
-    email: "",
-    orderNumber: "",
-    message: "",
-  });
+  const [formFields, setFormFields] = useState({ ...defaults });
+  const [message, setMessage] = useState(["", false]);
 
   const changeHandler = ({ target: { name, value } }) => {
     setFormFields((prev) => ({ ...prev, [name]: value }));
   };
-  const submitHandler = () => {};
+
+  const clearHandler = () => {
+    setFormFields({ ...defaults });
+  };
+  const submitHandler = () => {
+    console.log(formFields);
+    setMessage(["Message Sent", true]);
+    clearHandler();
+  };
+
+  const valid = () => {
+    const { name, email, message } = formFields;
+    return name && email && message ? false : true;
+  };
 
   return (
     <Box
@@ -48,8 +58,17 @@ export default function Form() {
         onChange={changeHandler}
       ></TextField>
       <Box>
-        <Button onClick={submitHandler}>Submit</Button>
+        <Button onClick={clearHandler}>Reset</Button>
+        <Button onClick={submitHandler} disabled={valid()}>
+          Submit
+        </Button>
       </Box>
+      <Snackbar
+        open={message[1]}
+        message={message[0]}
+        autoHideDuration={5000}
+        onClose={() => setMessage(["", false])}
+      ></Snackbar>
     </Box>
   );
 }
